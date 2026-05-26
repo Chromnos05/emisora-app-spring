f5a30970702535547d01ea15d5fad774b408c93d
Oscar Mercado <omercador@unicartagena.edu.co>
2026-05-26 18:31:52 -0500

    init: configuración inicial del proyecto Spring Boot

---
+++ emisora-app/src/main/java/com/udc/emisora_app/EmisoraAppApplication.java
@@ -0,0 +1,13 @@
+package com.udc.emisora_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmisoraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmisoraAppApplication.class, args);
+	}
+
+}
---
+++ emisora-app/src/test/java/com/udc/emisora_app/EmisoraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udc.emisora_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmisoraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
